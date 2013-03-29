b5a3835e4a9b0bf76c2ea6956f7a35189815d08e
toscho <info@toscho.de>
2013-03-29 23:10:07 +0100

    add plugin URI

--- t5-spam-block.php
+++ t5-spam-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: T5 Spam Block
  * Description: Block spam before it reaches the database.
- * Plugin URI:
+ * Plugin URI:  https://github.com/toscho/T5-Spam-Block
  * Version:     2013.03.29
  * Author:      Thomas Scholz
  * Author URI:  http://toscho.de
